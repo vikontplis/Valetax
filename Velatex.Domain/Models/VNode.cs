095eb31da73dab46be4f0ce21fd7c2cbd327b42e
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 10:48:47 +0300

    add node model

---
+++ Velatex.Domain/Models/VNode.cs
@@ -0,0 +1,10 @@
+﻿namespace Velatex.Domain.Models;
+
+public class VNode
+{
+    public long Id { get; set; }
+    public string Name { get; set; } 
+    public long ParentId { get; set; }
+    public  VNode Parent { get; set; }
+    public  IEnumerable<VNode> Children { get; set; }
+}
