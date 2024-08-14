6c2766710e4113feab904c50a687176d5af8a1a7
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 21:12:43 +0300

    fix db migration

---
+++ Valetax.Infrastructure/Migrations/20240814180337_fluentModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Valetax.Infrastructure.Migrations
+{
+    public partial class fluentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ParentId",
+                table: "Nodes",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ParentId",
+                table: "Nodes",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
--- Velatex.Domain/Models/VNode.cs
+++ Velatex.Domain/Models/VNode.cs
@@ -4,7 +4,7 @@ public class VNode
 {
     public long Id { get; set; }
     public string Name { get; set; } 
-    public long ParentId { get; set; }
+    public long? ParentId { get; set; }
     public virtual VNode Parent { get; set; }
     public virtual IEnumerable<VNode> Children { get; set; }
 }
