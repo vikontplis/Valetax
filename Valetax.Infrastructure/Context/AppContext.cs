ae641b8f2a43450b3de32f747e980cfb73a3b33f
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 12:32:16 +0300

    setup app context

--- Valetax.Infrastructure/Context/AppContext.cs
+++ Valetax.Infrastructure/Context/AppContext.cs
@@ -3,9 +3,17 @@ using Velatex.Domain.Models;
 
 namespace Valetax.Infrastructure.Context;
 
-public class AppContext: DbContext
+public class AppContext : DbContext
 {
-    public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+    public AppContext(DbContextOptions<AppContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5433;Database=valetax;Username=postgres;Password=plisleopas");
+    }
 
     public DbSet<VNode> Nodes { get; set; }
 }
