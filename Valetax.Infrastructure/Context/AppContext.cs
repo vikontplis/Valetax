e9bacb6be4fdd1170a3415b33215783796bc8b7c
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 13:10:32 +0300

    fix pg port

--- Valetax.Infrastructure/Context/AppContext.cs
+++ Valetax.Infrastructure/Context/AppContext.cs
@@ -8,7 +8,7 @@ public class AppDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql(
-            "Host=localhost;Port=5433;Database=valetax;Username=postgres;Password=plisleopas");
+            "Host=localhost;Port=5432;Database=valetax;Username=postgres;Password=plisleopas");
     }
 
     public DbSet<VNode> Nodes { get; set; }
