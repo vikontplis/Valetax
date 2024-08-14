810ec30fe0dc8a0911c217bfebaab9e0d62ed29c
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 13:00:02 +0300

    fix connection string

--- Valetax.Infrastructure/Context/AppContext.cs
+++ Valetax.Infrastructure/Context/AppContext.cs
@@ -3,9 +3,9 @@ using Velatex.Domain.Models;
 
 namespace Valetax.Infrastructure.Context;
 
-public class AppContext : DbContext
+public class AppDbContext : DbContext
 {
-    public AppContext(DbContextOptions<AppContext> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
