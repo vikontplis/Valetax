d603e33c395d64979a721dc6c2fbf6f2728852fb
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 17:46:49 +0300

    add user controller

---
+++ Valetax.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Valetax.WebAPI.Controllers;
+
+[ApiController]
+//[Route("api.[controller]")]
+[Produces("application/json")]
+public class UserController : ControllerBase
+{
+    /// <summary>
+    /// remember Me
+    /// </summary>
+    [HttpPost]
+    [Route("api.[controller].partner.[action]")]
+    public ActionResult<bool> RememberMe(string code)
+    {
+        return Ok();
+    }
+}
--- Valetax.WebAPI/Program.cs
+++ Valetax.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Valetax API",
+        Description = "An ASP.NET Core Web API for managing Tree items",
+        
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 /*
 builder.Services.AddDbContext<AppDbContext>(options =>
