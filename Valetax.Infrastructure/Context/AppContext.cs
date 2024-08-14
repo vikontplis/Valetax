5b436a9211b1a1f755cec549d979677ff189ffac
Leonid Plis <vikont.plis@gmail.com>
2024-08-14 12:23:33 +0300

    add db context

---
+++ Valetax.Infrastructure/Context/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Velatex.Domain.Models;
+
+namespace Valetax.Infrastructure.Context;
+
+public class AppContext: DbContext
+{
+    public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+    public DbSet<VNode> Nodes { get; set; }
+}
