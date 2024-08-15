4dd703e8be4ee9dcb44790da57394cff9305afd8
Leonid Plis <vikont.plis@gmail.com>
2024-08-15 06:30:17 +0300

    make migration

---
+++ Valetax.Infrastructure/Migrations/20240815032914_AddReqInfo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Valetax.Infrastructure.Migrations
+{
+    public partial class AddReqInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "Journals",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Query",
+                table: "Journals",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Trace",
+                table: "Journals",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "Journals");
+
+            migrationBuilder.DropColumn(
+                name: "Query",
+                table: "Journals");
+
+            migrationBuilder.DropColumn(
+                name: "Trace",
+                table: "Journals");
+        }
+    }
+}
